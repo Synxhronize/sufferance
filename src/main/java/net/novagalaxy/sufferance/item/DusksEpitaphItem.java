15025cb04cdc8b7531d78d3ef7611a7ae0c8b7ef
Synxhronize <adminbyte@localhost>
2024-12-08 07:30:28 -0600

    Changed Dusk's Epitaph render (broken), added self death ban (for some reason), added advancement for crafting Epitaph (not implemented)

--- src/main/java/net/novagalaxy/sufferance/item/DusksEpitaphItem.java
+++ src/main/java/net/novagalaxy/sufferance/item/DusksEpitaphItem.java
@@ -1,14 +1,20 @@
 
 package net.novagalaxy.sufferance.item;
 
+import net.novagalaxy.sufferance.procedures.DusksEpitaphRightclickedProcedure;
+import net.novagalaxy.sufferance.procedures.DusksEpitaphItemIsCraftedsmeltedProcedure;
 import net.novagalaxy.sufferance.init.SufferanceModTabs;
 
+import net.minecraft.world.level.Level;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.InteractionHand;
 
 public class DusksEpitaphItem extends SwordItem {
 	public DusksEpitaphItem() {
@@ -37,5 +43,18 @@ public class DusksEpitaphItem extends SwordItem {
 				return Ingredient.of(new ItemStack(Items.NETHERITE_INGOT));
 			}
 		}, 3, -2.7f, new Item.Properties().tab(SufferanceModTabs.TAB_SUFFERANCE).fireResistant());
+	}
+
+	@Override
+	public InteractionResultHolder<ItemStack> use(Level world, Player entity, InteractionHand hand) {
+		InteractionResultHolder<ItemStack> ar = super.use(world, entity, hand);
+		DusksEpitaphRightclickedProcedure.execute(world, entity);
+		return ar;
+	}
+
+	@Override
+	public void onCraftedBy(ItemStack itemstack, Level world, Player entity) {
+		super.onCraftedBy(itemstack, world, entity);
+		DusksEpitaphItemIsCraftedsmeltedProcedure.execute(entity);
 	}
 }
